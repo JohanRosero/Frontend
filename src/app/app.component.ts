a00ee01c51728128a69ea76bef3292f488ee79c7
JOHAN CRISTHIAN ROSERO ROSERO <130606281+JohanRosero@users.noreply.github.com>
2024-10-02 20:36:15 -0300

    Configuraciones Iniciales, componentes y despliegue del proyecto Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from "./components/footer/footer.component";
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterModule,RouterOutlet, HeaderComponent, FooterComponent,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
